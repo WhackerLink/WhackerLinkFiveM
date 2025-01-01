37431d9787a6aa3af31bd619f2dd590fa51d218e
firealarmss <caleb.k4php@gmail.com>
2025-01-01 01:38:04 -0600

    Remove large commented code section as I do not think we will use it

--- client/ui/js/script.js
+++ client/ui/js/script.js
@@ -14,7 +14,7 @@
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *
-* Copyright (C) 2024 Caleb, K4PHP
+* Copyright (C) 2024-2025 Caleb, K4PHP
 *
 */
 
@@ -174,39 +174,6 @@ function startCheckLoop() {
             }
         }
     }, 5000);
-
-    /*    groupGrantCheckInterval = setInterval(() => {
-            // if (isVoiceRequested && !isVoiceGranted && !isVoiceGrantHandled) {
-            //     document.getElementById("rssi-icon").src = `models/${radioModel}/icons/tx.png`;
-            //     SendGroupVoiceRequest();
-            //
-            //     if (!isVoiceGranted && isVoiceRequested) {
-            //         setTimeout(() => {
-            //             document.getElementById("rssi-icon").src = `models/${radioModel}/icons/rssi${currentRssiLevel}.png`;
-            //         }, 400);
-            //     }
-            //
-            //     setTimeout(() => {
-            //         if (!isVoiceGranted && !isTxing && isVoiceRequested) {
-            //             isVoiceRequested = false;
-            //             isVoiceGranted = false;
-            //             isTxing = false;
-            //             bonk();
-            //         }
-            //     }, 3000);
-            // }
-
-            if (isVoiceGranted && isTxing && !isVoiceGrantHandled) {
-                document.getElementById("rssi-icon").src = `models/${radioModel}/icons/rssi${currentRssiLevel}.png`;
-                isVoiceRequested = false;
-                isVoiceGranted = true;
-                setTimeout(() => {
-                    tpt_generate();
-                    document.getElementById("rssi-icon").src = `models/${radioModel}/icons/tx.png`;
-                }, 250);
-                isVoiceGrantHandled = true;
-            }
-        }, 300);*/
 }
 
 function stopCheckLoop() {
